4eca19c38eaf804bebfe2999f0600e85f5c2c6a7
Bezuro <serfaksion25@gmail.com>
2023-03-15 20:29:22 +0200

    Added MUI theme

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+    background: {
+      default: '#212121',
+      paper: '#424242',
+    },
+  },
+});
+
+export default theme;
